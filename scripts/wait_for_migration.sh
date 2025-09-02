298692f7666e2f3e0bc861c0dacf569715dfcf97
Ritik Jain <naijkitir@gmail.com>
2025-09-02 23:04:29 +0530

    [BUILD] fix migration waiter

--- scripts/wait_for_migration.sh
+++ scripts/wait_for_migration.sh
@@ -3,10 +3,9 @@ set -e
 
 echo "Waiting for database migrations ..."
 
-# Change to app directory
-cd /app
-
 # Run the Node.js waiter script
-node scripts/wait_for_migration.js "$@"
+cd /app/package
+export NODE_PATH=/app/packages/db/node_modules
+node ../../scripts/wait_for_migration.js "$@"
 
 echo "Migration waiter completed successfully"
